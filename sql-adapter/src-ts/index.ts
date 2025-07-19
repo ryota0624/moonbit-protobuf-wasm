3f2adb0bbab88941e5a0ae3f3a195d2b0bbe27b4
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-07-19 20:40:50 +0900

    impl param passing

--- sql-adapter/src-ts/index.ts
+++ sql-adapter/src-ts/index.ts
@@ -1,45 +1,84 @@
-import * as sql_adapter from "../target/js/release/build/sql-adapter"
-import { RunQueryResult_ResultType, RunQueryResultSchema, RunQuerySchema, Value_ValueType } from "../gen-ts/query_pb";
+import * as sql_adapter from "../target/js/release/build/sql-adapter.js"
+import { RunQueryResult_ResultType, RunQueryResultSchema, RunQuerySchema, Value_ValueType } from "../gen-ts/query_pb.js";
 import * as protobuf from "@bufbuild/protobuf";
 import { Unit } from "../target/js/release/build/moonbit.js";
+import { Database } from "bun:sqlite";
+
+
 class JsAdapter {
+
 	async prepareContext(connect: string) {
-		console.log(connect);
-		return new JsRunQueryContext()
+		const dbFileName = 'users.sqlite'
+		const db = new Database(dbFileName, { create: true })
+
+		const tableName = 'users'
+		db.query('CREATE TABLE IF NOT EXISTS ' + tableName + ' (name VARCHAR(255), age INTEGER)').run()
+		return new JsRunQueryContext(db)
 	}
 	async runQuery(ctx: JsRunQueryContext, bytes: Uint8Array) {
 		const runQueryCommand = protobuf.fromBinary(RunQuerySchema, bytes)
 		console.log(ctx, runQueryCommand);
+
+		const queryParams = runQueryCommand.values.map((value) => {
+			switch (value.type) {
+				case Value_ValueType.UNSPECIFIED:
+					throw Error("value type unspecified")
+				case Value_ValueType.STRING:
+					return value.str
+				case Value_ValueType.INT:
+					return value.int
+			}
+		});
+
+		const query = ctx.database?.query(runQueryCommand.query)
+		const queryResultRows = query?.all(...queryParams)?.map((row) => {
+			const columns = Object.entries(row as Object).map((column) => {
+				const value = (() => {
+					/// TODO: null support
+					switch (typeof column[1]) {
+						case "string":
+							return {
+								type: Value_ValueType.STRING,
+								str: column[1] as string,
+							
+							}
+						case "number":
+							return {
+								type: Value_ValueType.INT,
+								int: column[1] as unknown as bigint,
+							}
+						default:
+							console.log(column)
+							throw Error(`unsupproted column type ${typeof column[1]}`)
+					}
+				})()
+
+				return {
+					name: column[0],
+					value: value,
+				}
+			})
+
+			return {
+				columns,
+			}
+		})
+
 		const result = protobuf.create(RunQueryResultSchema, {
 			resultType: RunQueryResult_ResultType.RESULT_SUCCESS,
 			resultSet: {
-				rows: [
-					{
-						columns: [
-							{
-								name: "age",
-								value: {
-									type: Value_ValueType.INT,
-									int: 1000 as unknown as bigint
-								}
-							},
-							{
-								name: "name",
-								value: {
-									type: Value_ValueType.STRING,
-									str: "taro"
-								}
-							},
-						]
-					}
-				]
+				rows: queryResultRows,
 			}
 		})
-		return protobuf.toBinary(RunQueryResultSchema, result)
+		return (protobuf.toBinary(RunQueryResultSchema, result))
+
+
 	}
 }
 
-class JsRunQueryContext { }
+class JsRunQueryContext {
+	constructor(public database?: Database) { }
+}
 
 sql_adapter.setup_JsAdapter(new JsAdapter())
 
